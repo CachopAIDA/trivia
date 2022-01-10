96e09ffa2e19928304d63bf864c56d76b76c67cf
Oscar Herrero <oherrero@aidacanarias.com>
2022-01-10 16:22:46 +0100

    💥 Generate_golden_master_files and Verify_file_content

--- C#/Trivia/Tests/GoldenMasterTest.cs
+++ C#/Trivia/Tests/GoldenMasterTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using Trivia;
 using Xunit;
 
@@ -27,5 +29,37 @@ namespace Tests
 					File.ReadAllText(Path.Combine(folder, "trivialOutput.1.txt"))
 			);
 		}
+
+        [Fact]
+        public void Generate_golden_master_files()
+        {
+            const string folder = @"../../../OutputGM";
+            Directory.CreateDirectory(folder);
+
+            for (var i = 0; i <= 1000; i++)
+            {
+                var file = $"trivialOutput.{i}.txt";
+                using var fileStream = new FileStream(Path.Combine(folder, file), FileMode.Create);
+                using var outputStream = new StreamWriter(fileStream);
+                Console.SetOut(outputStream);
+                GameRunner.PlayGame(new Random(i));
+            }
+        }
+
+        [Fact]
+        public void Verify_file_content()
+        {
+            const string folder = @"../../../OutputGM";
+            for (var i = 0; i <= 1000; i++)
+            {
+                var file = $"trivialOutput.{i}.txt";
+
+                var contentEspec = new StringBuilder();
+                Console.SetOut(new StringWriter(contentEspec));
+
+                GameRunner.PlayGame(new Random(i));
+                Assert.Equal(contentEspec.ToString(), File.ReadAllText(Path.Combine(folder, file)));
+            }
+        }
 	}
 }
